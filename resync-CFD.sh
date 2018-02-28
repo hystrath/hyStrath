64ed13f962898fac468c4f09b8355e26b6700ef6
Vincent Casseau <vincent.casseau@strath.ac.uk>
2018-02-28 16:27:13 +0000

    Parallel installation

--- resync-CFD.sh
+++ resync-CFD.sh
@@ -7,63 +7,68 @@ userName=`whoami`
 currentDir=`pwd`
 sendingDir="$HOME/$WM_PROJECT/$userName-$WM_PROJECT_VERSION"
 
+nProcs=1
+if [ $# -ne 0 ]
+  then nProcs=$1;
+fi
+
 mkdir -p $sendingDir
 
 # synchronize folders and compile new libraries -------------------------------
 rsync -rtvuc $currentDir/src/thermophysicalModels/strath/ $sendingDir/src/thermophysicalModels/strath/
 cd $sendingDir/src/thermophysicalModels/strath/
-./Allwmake
+./Allwmake -j$nProcs
 
 rsync -rtvuc $currentDir/src/TurbulenceModels/compressible/ $sendingDir/src/TurbulenceModels/compressible/
 cd $sendingDir/src/TurbulenceModels/compressible
-wmake libso
+wmake -j$nProcs libso
 rsync -rtvuc $currentDir/src/TurbulenceModels/schemes/ $sendingDir/src/TurbulenceModels/schemes/
 cd $sendingDir/src/TurbulenceModels/schemes
-wmake libso
+wmake -j$nProcs libso
 
 cd $sendingDir/src/thermophysicalModels/strath/
-./AllwmakeBis
+./AllwmakeBis -j$nProcs
 
 rsync -rtvuc $currentDir/src/hTCModels/ $sendingDir/src/hTCModels/
 cd $sendingDir/src/hTCModels
-wmake libso
+wmake -j$nProcs libso
 
 rsync -rtvuc $currentDir/src/finiteVolume/ $sendingDir/src/finiteVolume/
 cd $sendingDir/src/finiteVolume
-wmake libso
+wmake -j$nProcs libso
 
 rsync -rtvuc $currentDir/src/functionObjects/ $sendingDir/src/functionObjects/
 cd $sendingDir/src/functionObjects
-./Allwmake-hyStrath
+./Allwmake-hyStrath -j$nProcs
 
 rsync -rtvuc $currentDir/src/fvOptions/ $sendingDir/src/fvOptions/
 cd $sendingDir/src/fvOptions
-wmake libso
+wmake -j$nProcs libso
 
 
 # compile new executables ------------------------------------------------------
 #---- solvers ----
 rsync -rtvuc $currentDir/applications/solvers/compressible/hy2Foam/ $sendingDir/applications/solvers/compressible/hy2Foam/
 cd $sendingDir/applications/solvers/compressible/hy2Foam/
-./Allwmake
+./Allwmake -j$nProcs 
 
 #---- utilities ----
 rsync -rtvuc $currentDir/applications/utilities/generation/makeAxialMesh $sendingDir/applications/utilities/generation/makeAxialMesh
 cd $sendingDir/applications/utilities/generation/makeAxialMesh
-wmake
+wmake -j$nProcs 
 
 rsync -rtvuc $currentDir/applications/utilities/generation/blockMeshDG $sendingDir/applications/utilities/generation/blockMeshDG
 cd $sendingDir/applications/utilities/generation/blockMeshDG
-./Allwmake
+./Allwmake -j$nProcs 
 
 rsync -rtvuc $currentDir/applications/utilities/postProcessing/wall $sendingDir/applications/utilities/postProcessing/wall
 cd $sendingDir/applications/utilities/postProcessing/wall
-./wmakeAll
+./wmakeAll -j$nProcs 
 
 
 # re-set to the initial directory ---------------------------------------------
 cd $currentDir
 
-echo -e "
+echo "
 cfdStrath_$WM_PROJECT_VERSION updated successfully. Hope you'll enjoy it, $userName :)
 "
