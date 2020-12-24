hystrath/hyStrath

commit 9352e04828645e5de6c518f7c6df0f95e776815c

hyStrath: updating run/ headers

run/hyStrath/hy2MhdFoam/NASA_MSL_forebody_NR-MHD/calculateBMSL.C
@@ -164,7 +164,7 @@ int main()
   fprintf(B_file, "/*--------------------------------*- C++ -*----------------------------------*\\\n\
 | =========                 |                                                 |\n\
 | \\\\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox           |\n\
-|  \\\\    /   O peration     | Version:  v1706                                 |\n\
+|  \\    /   O peration     | Version:  v1706                                 |
 |   \\\\  /    A nd           | Web:      www.OpenFOAM.com                      |\n\
 |    \\\\/     M anipulation  |                                                 |\n\
 \\*---------------------------------------------------------------------------*/\n\

run/hyStrath/hy2Foam/cylinderReactingMach20/Allclean
@@ -1,10 +1,0 @@
-#!/bin/sh
-cd ${0%/*} || exit 1    # run from this directory
-
-# Source tutorial clean functions
-. $WM_PROJECT_DIR/bin/tools/CleanFunctions
-
-cleanCase
-cleanSamples
-
-find . -name "*~" -delete

run/hyStrath/hy2Foam/cylinderReactingMach20/Allrun
@@ -1,25 +1,0 @@
-#!/bin/bash
-cd ${0%/*} || exit 1    # run from this directory
-
-nProcs=1
-if [ $# -ne 0 ] ; then
-  if ! [[ $nProcs =~ '^[0-9]' ]]
-     then nProcs=$1;
-  fi
-fi
-
-# Source tutorial run functions
-. $WM_PROJECT_DIR/bin/tools/RunFunctions
-
-cp -r ./constant/backup-polyMesh ./constant/polyMesh
-runApplication checkMesh
-
-if [ $nProcs -eq 1 ] ; then
-  runApplication hy2Foam
-else
-  runApplication decomposePar -latestTime
-  mpirun -np $nProcs hy2Foam -parallel > log.hy2Foam 2>&1
-  runApplication reconstructPar -latestTime
-fi
-
-hy2Foam -postProcess -dict 'system/surfaceCoefficients' -latestTime

commit 601f2bd305b4aa7720b3aa88a2a9e643c6b2285c

hy2MhdFoam: small correction to the magnetic field C program (the error was introduced in commit 9352e04).

run/hyStrath/hy2MhdFoam/NASA_MSL_forebody_NR-MHD/Allclean
@@ -8,4 +8,4 @@ cleanCase
 cleanSamples
 
 find . -name "*~" -delete
-rm calculateB_MSL centers coil
+rm -f calculateB_MSL centers coil

run/hyStrath/hy2MhdFoam/NASA_MSL_forebody_NR-MHD/Allrun
@@ -15,7 +15,7 @@ runApplication createPatch -overwrite
 postProcess -func writeCellCentres -time '0' > /dev/null 2>&1
 gcc calculateBMSL.C -o calculateB_MSL -lm
 ./calculateB_MSL > /dev/null 2>&1
-rm 0/C 0/Cx 0/Cy 0/Cz
+rm -f 0/C 0/Cx 0/Cy 0/Cz
 
 cp -r backup_NR_0.0014 0.0014
 mv 0/B 0.0014/

run/hyStrath/hy2MhdFoam/NASA_MSL_forebody_NR-MHD/calculateBMSL.C
@@ -164,7 +164,7 @@ int main()
   fprintf(B_file, "/*--------------------------------*- C++ -*----------------------------------*\\\n\
 | =========                 |                                                 |\n\
 | \\\\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox           |\n\
-|  \\    /   O peration     | Version:  v1706                                 |
+|  \\    /   O peration     | Version:  v1706                                 |\n\
 |   \\\\  /    A nd           | Web:      www.OpenFOAM.com                      |\n\
 |    \\\\/     M anipulation  |                                                 |\n\
 \\*---------------------------------------------------------------------------*/\n\